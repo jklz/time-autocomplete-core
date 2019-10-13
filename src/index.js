6589e2e5cd5322e84cc2f61386fbc4684aa82e3e
Jared Spencer <jklz@me.com>
2019-10-12 22:09:12 -0600

    updated comments

--- src/index.js
+++ src/index.js
@@ -1,53 +1,70 @@
 
 const TimeHourOptions  = require("./.internal/TimeHourOptions.json");
 
-const defaultProps = {
-    min: null,
-    max: null,
-    is24: false,
-    currentOptions: [],
-};
 class TimeAutocompleteCore {
 
-    constructor(namedProperties) {
-        const config = {
-            min: null,
-            max: null,
-            is24: false,
-            ...namedProperties,
+    /**
+     *
+     * @param Integer min
+     * @param Integer max
+     * @param Boolean is24
+     */
+    constructor({min, max, is24}) {
+        /** @var Object configuration for object */
+        this.config = {
+            min,
+            max,
+            is24: !!is24,
         };
-        super(config);
         this.currentOptions = [];
     }
 
+    /**
+     * update config for min time
+     * @param String newValue
+     */
     setMin(newValue) {
-        this.min = newValue || null;
+        this.config.min = newValue || null;
     }
 
+    /**
+     * update config for max time
+     * @param String newValue
+     */
     setMax(newValue) {
-        this.max = newValue|| null;
+        this.config.max = newValue|| null;
     }
 
+    /**
+     * update config for if 24 hour format supported
+     * @param Boolean newValue
+     */
     setIs24(newValue) {
-        this.is24 = !!newValue;
+        this.config.is24 = !!newValue;
     }
 
+    /**
+     * update config for all properties passed
+     * @param Object configChanges
+     */
     loadConfig(configChanges) {
         if (!configChanges) {
             /** nothing to change */
             return;
         }
         Object.keys(configChanges).map((configKey) => {
-            this[configKey] = configChanges[configKey];
-        })
-
+            this.config[configKey] = configChanges[configKey];
+        });
     }
 
+    /**
+     * get time options for display
+     * @param String input - current user input time
+     * @param Object config - any changes to current config
+     */
     getOptions(input, config) {
         this.loadConfig(config);
         
-
     }
-
 
 }
