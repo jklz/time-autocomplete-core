040121839b5a4e90c51768b75e37bf31c7cdd93c
Jared Spencer <jklz@me.com>
2019-10-12 21:03:54 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+
+import TimeHourOptions from "./.internal/TimeHourOptions";
+
+const defaultProps = {
+    min: null,
+    max: null,
+    is24: false,
+    currentOptions: [],
+};
+class TimeAutocompleteCore {
+
+    constructor(namedProperties) {
+        const config = {
+            min: null,
+            max: null,
+            is24: false,
+            ...namedProperties,
+        };
+        super(config);
+        this.currentOptions = [];
+    }
+
+    setMin(newValue) {
+        this.min = newValue || null;
+    }
+
+    setMax(newValue) {
+        this.max = newValue|| null;
+    }
+
+    setIs24(newValue) {
+        this.is24 = !!newValue;
+    }
+
+    loadConfig(configChanges) {
+        if (!configChanges) {
+            /** nothing to change */
+            return;
+        }
+        Object.keys(configChanges).map((configKey) => {
+            this[configKey] = configChanges[configKey];
+        })
+
+    }
+
+    getOptions(input, config) {
+        this.loadConfig(config);
+        
+
+    }
+
+
+}
