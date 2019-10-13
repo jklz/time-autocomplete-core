79dffa2d41ed8a18120e23b17ea461a50737f103
Jared Spencer <jklz@me.com>
2019-10-13 02:14:43 -0600

    started on value processing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+import _ from 'lodash';
 const TimeHourOptions  = require("./.internal/TimeHourOptions.json");
 
 export default class  TimeAutocompleteCore {
@@ -75,7 +76,47 @@ export default class  TimeAutocompleteCore {
      */
     getOptions(input, config) {
         this.loadConfig(config);
+        /** get min and max as {hour, min} */
+        let min = null;
+        if(this.config.min && this.config.min.length){
+            min = {
+                hour: "00",
+                min: "00",
+            };
+        }
+        let max = null;
+        if(this.config.max && this.config.max.length) {
+            max = {
+                hour: "23",
+                min: "59",
+            };
+        }
 
+        /** trim and remove space from input */
+        input = (input || "").toLowerCase().trim();
+        /** build possible search options from input */
+        const inputSearchMain = /^([0-2]?[0-9]{1})[\s:;]?([0-5][0-9]?)\s?(a|am|p|pm)?$/.exec(input);
+        const inputSearchSub = /^([0-2]?[0-9]{1})[\s:;]?([0-5][0-9])\s?(a|am|p|pm)?$/.exec(input) || [];
+
+        /** @var array */
+        let results = [];
+
+        /**
+         * @var Array availableHours
+         */
+        let availableHours = TimeHourOptions;
+
+        /** if min and max are set, we will filter available to fit */
+        if (!!min || !!max) {
+            /**
+             * @var Object option
+             */
+           availableHours = availableHours.filter((option) => {
+               if (!!min && min.hour > option.value24) {
+                   return false
+               }
+           })
+        }
     }
 
 }
