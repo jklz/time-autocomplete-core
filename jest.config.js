4970038e2396b98365a3eaf5fbb9eb9a9a322fb3
Jared Spencer <jklz@me.com>
2019-10-12 23:46:29 -0600

    updated coverage test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
     "collectCoverageFrom": [
         "**/*.js",
         "!**/node_modules/**",
+        "!**/*.config.js",
+        "!**/*.test.js",
         "!**/coverage/**",
         "!**/vendor/**"
     ]
